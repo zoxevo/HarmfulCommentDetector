ee97e54644e8c21955066425efca9dab51d42911
Ghosteo <123637805+zoxevo@users.noreply.github.com>
2025-12-24 12:23:22 +0330

    اصلاح کد بارگذاری داده و رفع مشکلات نمایش
    برای فایل های PHICAD مشکل جدا کننده اشتباه حل شد(تب به جای گاما).
    برای پشتیبانی درست از متن های فارسی هم UTF-8 رو اضافه کردیم.
    مشکل نمایش ستون ها هم رفع کردیم.

--- explore_data.py
+++ explore_data.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
-df1 = pd.read_csv('PHICAD-part1.csv')
-df2 = pd.read_csv('PHICAD-part2.csv')
+df1 = pd.read_csv('PHICAD-part1.csv', sep='\t', encoding='utf-8', skiprows=[1])
+df2 = pd.read_csv('PHICAD-part2.csv', sep='\t', encoding='utf-8', skiprows=[1])
 
-print(df1.head())
-print(df1.columns)
-print(len(df1))
+df= pd.concat([df1, df2], ignore_index=True)
+
+print(df.head())
+print(df.columns)
+print(len(df))
+
+print("\nتوزیع لیبل‌ها در دیتاست:")
+print(df['class'].value_counts())
